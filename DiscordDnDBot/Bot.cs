5911a1a3b40bdd6d287104070b7ac0b9de4c23f1
Aidan Pinard <31970261+aidanpinard@users.noreply.github.com>
2022-01-23 19:15:01 -0400

    Prepared Basic Folder Setup.

--- DiscordDnDBot/Bot.cs
+++ DiscordDnDBot/Bot.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace DiscordDnDBot
 {
-    public class Program
+    internal class Bot
     {
-
     }
 }
--- DiscordDnDBot/Types/DiscordLimits.cs
+++ DiscordDnDBot/Types/DiscordLimits.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DiscordDnDBot
+namespace DiscordDnDBot.Types
 {
-    public class Program
+    internal class DiscordLimits
     {
-
     }
 }
